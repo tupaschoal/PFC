1da9ba645d1d4c636df09a8c7f1ef62c76126c4a
Tulio Leao <tupaschoal@gmail.com>
2016-04-12 19:20:10 -0300

    Export execution log to file

--- PFC.py
+++ PFC.py
@@ -6,7 +6,7 @@ import sys
 chosenProject = \
 "/home/tuliolinux/Downloads/systemc-2.3.1/examples/sysc/simple_fifo"
 
-# Goes to project folder, compiles and save log
+# Goes to project folder, compiles and saves log
 try:
     os.chdir(chosenProject)
 except OSError:
@@ -16,3 +16,16 @@ try:
     subprocess.run("make", shell=True, check=True)
 except subprocess.CalledProcessError:
     sys.exit("Failed to compile")
+
+try:
+    out = subprocess.run("./simple_fifo.x", shell=True, check=True, \
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        f = open('/tmp/cleanBuildLog','w+b')
+        f.write(out.stdout)
+        f.write(out.stderr)
+        f.close()
+    except OSError:
+        sys.exit("Failed to use file")
+except subprocess.CalledProcessError:
+    sys.exit("Failed to run")
