1324ebbc5dbd24ecd63db815f5f061b979373bb4
Tulio Leao <tupaschoal@gmail.com>
2016-04-11 21:41:10 -0300

    Base file for PFC

---
+++ PFC.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import subprocess
+import os
+
+chosenProject = "/home/tuliolinux/Downloads/systemc-2.3.1/examples/sysc/"
+os.chdir(chosenProject)
+#subprocess.call(["cd", "-1", F])
+subprocess.call("ls")
+
