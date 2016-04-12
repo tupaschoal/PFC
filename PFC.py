3381d3b594be62abd781e261904f1d376a249442
Tulio Leao <tupaschoal@gmail.com>
2016-04-12 18:27:25 -0300

    Add make error treatment

--- PFC.py
+++ PFC.py
@@ -2,7 +2,13 @@
 import subprocess
 import os
 
-chosenProject = "/home/tuliolinux/Downloads/systemc-2.3.1/examples/sysc/"
+chosenProject = \
+"/home/tuliolinux/Downloads/systemc-2.3.1/examples/sysc/simple_fifo"
+
+# Goes to project folder, compiles and save log
 os.chdir(chosenProject)
-subprocess.call("ls")
-subprocess.call("make")
+subprocess.run("ls", shell=True, check=True)
+try:
+    subprocess.run("make", shell=True, check=True)
+except subprocess.CalledProcessError as err:
+    print("Failed to compile")
